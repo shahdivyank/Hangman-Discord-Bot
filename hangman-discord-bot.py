f693f1743a46c14da7eb92204bf7170abd27ff3e
Divyank Shah <36285610+shahdivyank@users.noreply.github.com>
2021-08-04 17:25:41 -0700

    migrated to using discord commands

--- hangman-discord-bot.py
+++ hangman-discord-bot.py
@@ -1,97 +1,32 @@
 from random_word import RandomWords
 from PyDictionary import PyDictionary
-import discord
+from discord.ext import commands
 import os
 
-TOKEN = os.environ["TOKEN"]
+client = commands.Bot(command_prefix="!")
 
 random = RandomWords()
 dictionary = PyDictionary()
-client = discord.Client()
+# client = discord.Client()
 
 letters = []
 guesses = []
-message_count = 0
+lives = 0
+word = ""
+inGame = False
+
 
 def generateWord():
-    return random.get_random_word(hasDictionaryDef = "true")
+    return random.get_random_word(hasDictionaryDef="true")
 
-@client.event
-async def on_message(message):
-    channel = ""
-    
-    global message_count
-    message_count += 1
-
-    if message.content.lower() == "$play" and message_count == 1:
-        await message.channel.send("Welcome to Hangman Adventures! Once prompted enter a letter you think is in the mystery word! Type $guess your_guess to guess the entire word!")
-        letters = []
-        guesses = []
-        display = ""
-        channel = message.channel
-        word = generateWord()
-        while True:
-            try:
-                if "-" not in word and dictionary.meaning(word, disable_errors=True) != None:
-                    break
-            except:
-                pass
-            word = generateWord()
-        for letter in word:
-            letters.append("-")
-
-        def check(m):
-            return len(m.content) >= 1 and m.channel == channel
-
-        lives = 10
-
-        while True:
-            display = ""
-            for letter in letters:
-                display += letter + " "
-            await channel.send(display)
-            await message.channel.send("Enter Your Guess. Enter 0 to Give Up:")
-            msg = await client.wait_for('message', check=check)
-            guess = str(msg.content.lower())
-            if guess == "0":
-                break
-            if lives == 0:
-                await message.channel.send("Better luck next time!")
-                break
-            if guess in guesses:
-                await message.channel.send("You Have Already Guessed the letter " + guess)
-                continue
-            if guess.startswith("$guess"):
-                guessWord = guess.split()
-                if word == guessWord[1]:
-                    await message.channel.send("Congratulations you guessed the word right!")
-                    break
-                else:
-                    await message.channel.send("That was not correct!")
-            if (not guess.isalpha() or len(guess) > 1) and (not guess.startswith("$guess")):
-                await message.channel.send("Please only enter single letters.")
-                continue
-            guesses.append(guess)
-            if guess in word:
-                i = 0
-                for letter in word:
-                    if guess == word[i]:
-                        letters[i] = guess
-                    i += 1
-                await message.channel.send("Great Guessing!")
-            else:
-                lives -= 1
-                msg = str("Try Again. You have " + str(lives) + " wrong guesses before GAME OVER!")
-                await message.channel.send(msg)
-            if "-" not in letters:
-                await message.channel.send("Congratulations you got it!")
-                break
-
+def ending():
+    global inGame
+    if inGame:
+        inGame = False
         partsOfSpeechs = ["Verb", "Noun", "Adjective"]
         meaning_dict = dictionary.meaning(word)
         meaning_list = []
         meaning_str = ""
-
         for partsOfSpeech in partsOfSpeechs:
             try:
                 meaning_list.append(meaning_dict[partsOfSpeech])
@@ -108,7 +43,88 @@ async def on_message(message):
                 meaning_str += " " + str(count) + ". " + sub_meaning
                 count += 1
         msg = str("Your word was " + word + ". It means" + meaning_str)
-        await message.channel.send(msg)
-        message_count = 0
+        return msg
 
-client.run(TOKEN)
+@client.command()
+async def playHangman(ctx):
+    global letters
+    global guesses
+    global lives
+    global word
+    global display
+    global inGame
+
+    lives = 10
+
+    if inGame == False:
+        inGame = True
+        await ctx.send("Welcome to Hangman Adventures! Once prompted enter a letter using '!guess letter' you think is in the mystery word! Type '!guessWord your_guess' to guess the entire word!")
+
+        letters = []
+        guesses = []
+        display = ""
+
+        while True:
+            word = generateWord()
+            try:
+                if "-" not in word and dictionary.meaning(word, disable_errors=True) != None:
+                    break
+            except:
+                pass
+
+        for letter in word:
+            letters.append("-")
+
+        await ctx.send(display.join(letters) + "\nEnter a letter:")
+    else:
+        await ctx.send("A game is already in progress. Enter !end to stop the game!")
+
+
+@client.command()
+async def guess(ctx, guess_letter:str):
+    global lives
+    global word
+    global display
+    print(word)
+    if len(guess_letter) > 1:
+        await ctx.send("Enter single letters!")
+    elif not guess_letter.isalpha():
+        await ctx.send("Enter letters only")
+    elif guess_letter in guesses:
+        await ctx.send("You Have Already Guessed the letter " + guess_letter)
+    elif guess_letter.strip().lower() in word:
+        index = 0
+        for letter in word:
+            if letter == guess_letter:
+                letters[index] = guess_letter
+            index += 1
+        guesses.append(guess_letter)
+        await ctx.send("Great Guesing!\n" + display.join(letters) + "\nEnter a letter:")
+    elif "-" not in letters:
+        msg = ending()
+        inGame = False
+        await ctx.send("Congratulations you got it! " + msg)
+    elif lives == 0:
+        await ctx.send("Better luck next time!")
+    else:
+        await ctx.send("Incorrect Guess.\n" + display.join(letters) + "\nYou have " + str(lives) + "lives left. " + "Enter a letter:")
+        guesses.append(guess_letter)
+        lives -= 1
+
+@client.command()
+async def endgame(ctx):
+    msg = ending()
+    await ctx.send(msg)
+
+@client.command()
+async def guessWord(ctx, guess_word:str):
+    global lives
+    if guess_word == word:
+        msg = ending()
+        inGame = False
+        await ctx.send("Congratulations you guessed the word right! " + msg)
+    else:
+        await ctx.send("Incorrect Guess.\n" + display.join(letters) + "\nYou have " + str(lives) + " lives left. " + "Enter a letter:")
+        lives -= 1
+
+client.run(os.environ["TOKEN"])
